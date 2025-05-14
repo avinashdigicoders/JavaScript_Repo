0b30fa17b8dee5f991b587c42d1655053afd0700
AVINASH SINGH <avinashsing559@gmail.com>
2025-05-14 05:23:47 +0000

    math and num

---
+++ basic_js/06_number_and_math.js
@@ -0,0 +1,40 @@
+const number = 100
+// console.log(number);
+
+const balance = new Number(200)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.3349
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//------Maths++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //only change negative value in +
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(4.5));
+// console.log(Math.sqrt(5));
+// console.log(Math.sin(90));
+// console.log(Math.min(1,2,3,4,5,6,7,8,12,34,13));
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
+
+
+
+
+
+
+
+
+
